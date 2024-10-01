d90a01b74540de22f09fed29adf04291e048072c
noproto <noproto@zeroday.engineering>
2024-10-01 07:42:16 -0400

    Remove redundant code (already calculated by MFKey)

--- NestedSolver/progress.h
+++
@@ -1,9 +0,0 @@
-#ifndef MIFARE_LIB_PROGRESS_H
-#define MIFARE_LIB_PROGRESS_H
-#include <stdint.h>
-
-void print_progress(uint32_t count, uint32_t max);
-
-void exit_progress();
-
-#endif //MIFARE_LIB_PROGRESS_H
